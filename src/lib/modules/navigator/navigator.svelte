a65d4e92182f7f5bf74b1bdee94f9bc8ee47e544
data-miner00 <data-miner00@users.noreply.github.com>
2023-07-17 21:48:50 +0800

    wip: Add temporary navigator

---
+++ src/lib/modules/navigator/navigator.svelte
@@ -0,0 +1,4 @@
+<nav class="absolute top-5 left-5 p-2 border border-solid border-gray-500 rounded flex">
+	<a href="/" class="p-2 hover:bg-gray-200">Home</a>
+	<a href="/invoice" class="p-2 hover:bg-gray-200">Invoice</a>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
+	import Navigator from '$lib/modules/navigator/navigator.svelte';
 	import './styles.css';
 	import '../app.css';
 </script>
 
 <div class="app">
+	<Navigator />
 	<main>
 		<slot />
 	</main>
