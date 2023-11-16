e2ce18f308c439dfacd56f73616801246bf177c0
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-16 09:18:21 +0800

    feat: Update user profile

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,5 +1,6 @@
-import type { PageServerLoad } from '../$types';
+import type { PageServerLoad, Actions } from '../$types';
 import prisma from '$lib/prisma';
+import { error, fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ request }) => {
 	const profile = await prisma.user.findFirst();
@@ -20,3 +21,41 @@ export const load: PageServerLoad = async ({ request }) => {
 		bankAccount
 	};
 };
+
+export const actions = {
+	user: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const id = data.get('id')?.toString();
+		const username = data.get('username')?.toString();
+		const firstName = data.get('first_name')?.toString();
+		const lastName = data.get('last_name')?.toString();
+		const email = data.get('email')?.toString();
+		const phoneNo = data.get('phone')?.toString();
+		const password = data.get('password')?.toString();
+
+		if (!id || !username || !firstName || !lastName || !email || !phoneNo || !password) {
+			return fail(400, { message: 'bad input ' });
+		}
+
+		console.log(id);
+		try {
+			const updatedUser = await prisma.user.update({
+				where: {
+					id
+				},
+				data: {
+					username,
+					firstName,
+					lastName,
+					phoneNo,
+					password,
+					email
+				}
+			});
+			console.log(updatedUser);
+			return updatedUser;
+		} catch (e) {
+			throw error(500, `Error occurred: ${JSON.stringify(e)}`);
+		}
+	}
+} satisfies Actions;
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
 
 	export let data: PageData;
 
 	let profile = data.profile;
 	let address = data.address;
 	let bankAccount = data.bankAccount;
+
+	let profileDialogRef: HTMLDialogElement;
+	let addressDialogRef: HTMLDialogElement;
+	let bankDialogRef: HTMLDialogElement;
+
+	function showProfileDialog() {
+		profileDialogRef.show();
+	}
+
+	function closeProfileDialog() {
+		profileDialogRef.close();
+	}
 </script>
 
 <section class="px-4">
@@ -15,14 +28,22 @@
 
 	<div class="divider" />
 
+	<button class="btn" on:click={showProfileDialog}>Edit profile</button><br />
+
+	<label for="username" class="font-semibold">Username</label>
+	<div>{profile?.username}</div>
+
 	<label for="current-email" class="font-semibold">Email</label>
-	<div>alice@gmail.com</div>
+	<div>{profile?.email}</div>
 
 	<label for="current-email" class="font-semibold">Role</label>
 	<div>{profile?.role}</div>
 
 	<label for="current-email" class="font-semibold">Password</label>
 	<div>{profile?.password}</div>
+
+	<label for="current-email" class="font-semibold">Phone No.</label>
+	<div>{profile?.phoneNo}</div>
 
 	<label for="current-email" class="font-semibold">Created At</label>
 	<div>{profile?.createdAt}</div>
@@ -81,3 +102,99 @@
 </section>
 
 <div class="h-4" />
+
+<dialog bind:this={profileDialogRef} id="edit-profile-dialog" class="modal">
+	<div class="modal-box w-11/12 max-w-4xl">
+		<h3 class="font-bold text-lg">Edit Profile</h3>
+		<p class="py-4">Press ESC key or click the button below to close</p>
+
+		<form method="POST" action="?/user" use:enhance>
+			<!-- hidden input must not be disabled, else the value will not include in form data -->
+			<input name="id" type="text" class="hidden" value={profile?.id} required />
+
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="username">
+					<span class="label-text">Username</span>
+				</label>
+				<input
+					name="username"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={profile?.username}
+					required
+				/>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="first_name">
+					<span class="label-text">First name</span>
+				</label>
+				<input
+					name="first_name"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={profile?.firstName}
+					required
+				/>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="last_name">
+					<span class="label-text">Last name</span>
+				</label>
+				<input
+					name="last_name"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={profile?.lastName}
+					required
+				/>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="email">
+					<span class="label-text">Email</span>
+				</label>
+				<input
+					name="email"
+					type="email"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={profile?.email}
+					required
+				/>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="phone">
+					<span class="label-text">Phone</span>
+				</label>
+				<input
+					name="phone"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={profile?.phoneNo}
+					required
+				/>
+			</div>
+
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="password">
+					<span class="label-text">Password</span>
+				</label>
+				<input
+					name="password"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={profile?.password}
+					required
+				/>
+			</div>
+
+			<div class="modal-action">
+				<button class="btn btn-primary" on:click={closeProfileDialog}>Save</button>
+			</div>
+		</form>
+	</div>
+</dialog>
