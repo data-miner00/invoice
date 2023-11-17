8a3a96e253a7409941b2eff78946e94df7e1b149
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-18 07:52:47 +0800

    refactor: Componentize profile form dialog

---
+++ src/lib/modules/forms/update-profile-form.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	export let actionUrl: string;
+	export let profileId: string;
+	export let username: string;
+	export let firstName: string;
+	export let lastName: string;
+	export let email: string;
+	export let phoneNo: string;
+	export let password: string;
+
+	let profileDialogRef: HTMLDialogElement;
+
+	export function showProfileDialog() {
+		profileDialogRef.show();
+	}
+
+	function closeProfileDialog() {
+		profileDialogRef.close();
+	}
+</script>
+
+<dialog bind:this={profileDialogRef} id="edit-profile-dialog" class="modal">
+	<div class="modal-box w-11/12 max-w-4xl">
+		<h3 class="font-bold text-lg">Edit Profile</h3>
+
+		<form method="POST" action={actionUrl} use:enhance>
+			<!-- hidden input must not be disabled, else the value will not include in form data -->
+			<input name="id" type="text" class="hidden" value={profileId} required />
+
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="username">
+					<span class="label-text">Username</span>
+				</label>
+				<input
+					name="username"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={username}
+					required
+				/>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="first_name">
+					<span class="label-text">First name</span>
+				</label>
+				<input
+					name="first_name"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={firstName}
+					required
+				/>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="last_name">
+					<span class="label-text">Last name</span>
+				</label>
+				<input
+					name="last_name"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={lastName}
+					required
+				/>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="email">
+					<span class="label-text">Email</span>
+				</label>
+				<input
+					name="email"
+					type="email"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={email}
+					required
+				/>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="phone">
+					<span class="label-text">Phone</span>
+				</label>
+				<input
+					name="phone"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={phoneNo}
+					required
+				/>
+			</div>
+
+			<div class="form-control w-full max-w-xs">
+				<label class="label" for="password">
+					<span class="label-text">Password</span>
+				</label>
+				<input
+					name="password"
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+					value={password}
+					required
+				/>
+			</div>
+
+			<div class="modal-action">
+				<button class="btn btn-primary" on:click={closeProfileDialog}>Save</button>
+				<form on:submit|preventDefault={closeProfileDialog}>
+					<button class="btn btn-secondary">Close</button>
+				</form>
+			</div>
+		</form>
+	</div>
+</dialog>
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { enhance } from '$app/forms';
+	import UpdateProfileForm from '$lib/modules/forms/update-profile-form.svelte';
 
 	export let data: PageData;
 
@@ -8,17 +9,10 @@
 	let address = data.address;
 	let bankAccount = data.bankAccount;
 
-	let profileDialogRef: HTMLDialogElement;
+	let profileDialog: UpdateProfileForm;
+
 	let addressDialogRef: HTMLDialogElement;
 	let bankDialogRef: HTMLDialogElement;
-
-	function showProfileDialog() {
-		profileDialogRef.show();
-	}
-
-	function closeProfileDialog() {
-		profileDialogRef.close();
-	}
 </script>
 
 <section class="px-4">
@@ -28,7 +22,7 @@
 
 	<div class="divider" />
 
-	<button class="btn" on:click={showProfileDialog}>Edit profile</button><br />
+	<button class="btn" on:click={() => profileDialog.showProfileDialog()}>Edit profile</button><br />
 
 	<label for="username" class="font-semibold">Username</label>
 	<div>{profile?.username}</div>
@@ -103,98 +97,14 @@
 
 <div class="h-4" />
 
-<dialog bind:this={profileDialogRef} id="edit-profile-dialog" class="modal">
-	<div class="modal-box w-11/12 max-w-4xl">
-		<h3 class="font-bold text-lg">Edit Profile</h3>
-		<p class="py-4">Press ESC key or click the button below to close</p>
-
-		<form method="POST" action="?/user" use:enhance>
-			<!-- hidden input must not be disabled, else the value will not include in form data -->
-			<input name="id" type="text" class="hidden" value={profile?.id} required />
-
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="username">
-					<span class="label-text">Username</span>
-				</label>
-				<input
-					name="username"
-					type="text"
-					placeholder="Type here"
-					class="input input-bordered w-full max-w-xs"
-					value={profile?.username}
-					required
-				/>
-			</div>
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="first_name">
-					<span class="label-text">First name</span>
-				</label>
-				<input
-					name="first_name"
-					type="text"
-					placeholder="Type here"
-					class="input input-bordered w-full max-w-xs"
-					value={profile?.firstName}
-					required
-				/>
-			</div>
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="last_name">
-					<span class="label-text">Last name</span>
-				</label>
-				<input
-					name="last_name"
-					type="text"
-					placeholder="Type here"
-					class="input input-bordered w-full max-w-xs"
-					value={profile?.lastName}
-					required
-				/>
-			</div>
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="email">
-					<span class="label-text">Email</span>
-				</label>
-				<input
-					name="email"
-					type="email"
-					placeholder="Type here"
-					class="input input-bordered w-full max-w-xs"
-					value={profile?.email}
-					required
-				/>
-			</div>
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="phone">
-					<span class="label-text">Phone</span>
-				</label>
-				<input
-					name="phone"
-					type="text"
-					placeholder="Type here"
-					class="input input-bordered w-full max-w-xs"
-					value={profile?.phoneNo}
-					required
-				/>
-			</div>
-
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="password">
-					<span class="label-text">Password</span>
-				</label>
-				<input
-					name="password"
-					type="text"
-					placeholder="Type here"
-					class="input input-bordered w-full max-w-xs"
-					value={profile?.password}
-					required
-				/>
-			</div>
-
-			<div class="modal-action">
-				<button class="btn btn-primary" on:click={closeProfileDialog}>Save</button>
-			</div>
-		</form>
-	</div>
-</dialog>
+<UpdateProfileForm
+	bind:this={profileDialog}
+	actionUrl="?/user"
+	profileId={profile?.id ?? ''}
+	username={profile?.username ?? ''}
+	firstName={profile?.firstName ?? ''}
+	lastName={profile?.lastName ?? ''}
+	email={profile?.email ?? ''}
+	phoneNo={profile?.phoneNo ?? ''}
+	password={profile?.password ?? ''}
+/>
