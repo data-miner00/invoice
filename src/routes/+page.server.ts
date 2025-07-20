b434bc6bfb05bf0134946fa03b57841ac114b02c
Shaun Chong <data-miner00@users.noreply.github.com>
2025-07-20 10:24:40 +0800

    wip: Initial home page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,8 +3,14 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
 	const user = await prisma.user.findFirst();
+	const customers = await prisma.customer.findMany();
+	const addresses = await prisma.address.findMany();
+	const invoices = await prisma.invoice.findMany();
 
 	return {
-		user
+		user,
+		customers,
+		addresses,
+		invoices
 	};
 }) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,64 @@
 			loggedInUser$.set(data.user);
 		}
 	});
+
+	let invoiceVolume = data.invoices.reduce((acc, invoice) => acc + invoice.grandTotal, 0);
 </script>
 
-<div>Homepage</div>
+<div class="px-4">
+	<h1 class="text-3xl">Welcome {data.user?.firstName} {data.user?.lastName}</h1>
+	<div class="flex gap-10">
+		<div class="rounded bg-orange-300 px-10 py-6">
+			Customers - {data.customers.length}
+		</div>
+		<div class="rounded bg-blue-300 px-10 py-6">
+			Addresses - {data.addresses.length}
+		</div>
+		<div class="rounded bg-purple-300 px-10 py-6">
+			Invoices - {data.invoices.length}
+		</div>
+		<div class="rounded bg-yellow-300 px-10 py-6">
+			Invoices Total - {invoiceVolume.toLocaleString('en-US', {
+				style: 'currency',
+				currency: 'MYR'
+			})}
+		</div>
+	</div>
+
+	<div>
+		<h2 class="text-xl">Recent Invoices</h2>
+
+		<ul class="flex flex-col gap-1">
+			<li>
+				<div class="border border-solid border-black rounded h-10 w-32" />
+			</li>
+			<li>
+				<div class="border border-solid border-black rounded h-10 w-32" />
+			</li>
+			<li>
+				<div class="border border-solid border-black rounded h-10 w-32" />
+			</li>
+			<li>
+				<div class="border border-solid border-black rounded h-10 w-32" />
+			</li>
+		</ul>
+	</div>
+
+	<div>
+		<h2 class="text-xl">Audit Logs</h2>
+		<ul class="flex flex-col gap-1">
+			<li>
+				<div class="border border-solid border-black rounded h-10 w-32" />
+			</li>
+			<li>
+				<div class="border border-solid border-black rounded h-10 w-32" />
+			</li>
+			<li>
+				<div class="border border-solid border-black rounded h-10 w-32" />
+			</li>
+			<li>
+				<div class="border border-solid border-black rounded h-10 w-32" />
+			</li>
+		</ul>
+	</div>
+</div>
